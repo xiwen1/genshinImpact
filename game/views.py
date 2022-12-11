90da192583fb2ae160db1e4affbab40ee5e59574
xiwen_youmu <1597083201@qq.com>
2022-12-11 11:53:42 +0800

    first page

--- game/views.py
+++ game/views.py
@@ -12,7 +12,14 @@ print(context1)
 
 context2 = '''
 <h1 style="text-align: center">我是武汉大学张可为 qq：1597083201</h1>
+<a href="/lzkuainan"><h1 style="text-align: center">凌志快男</h1></a>
+<a href="/whu"><h1 style="text-align: center">武大传送门</h1></a>
 
+
+'''
+
+context3 = '''
+<h1 style="text-align: center; font-family: 宋体">凌志快男</h1>
 '''
 
 
@@ -21,5 +28,8 @@ def index(request):
 
 def index2(request):
     return HttpResponse(context1)
+
+def ding(request):
+    return HttpResponse(context3)
 
 
