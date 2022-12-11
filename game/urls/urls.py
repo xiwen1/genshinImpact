1ee1c7bd5fa06410bf39a46d50b56cca4248ef38
xiwen_youmu <1597083201@qq.com>
2022-12-11 18:37:38 +0800

    modify urls to perfect

--- game/urls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from game.views.views import index, index2, ding
-urlpatterns = [ # 这个app的url还需要写到总url中
-    path("", index, name="index"), # 如果url中后面什么都没有就会返回index这个函数
-    path("whu", index2, name="index2"),
-    path("lzkuainan", ding, name="ding")
-    
-]
--- game/views/index.py
+++ game/views/index.py
@@ -1,5 +1,17 @@
+import os
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from app.settings import BASE_DIR
+
+
+file_path = os.path.join(BASE_DIR, 'game/templates/multiends/index.html')
+
+
+
+def whu(request):
+    return render(request, "multiends/index.html")
 
 
 def index(request):
-    return render(request, "mutliends/web.html") # 总函数，整个游戏就着一个链接
+    return render(request, "multiends/web.html") # 总函数，整个游戏就着一个链接
