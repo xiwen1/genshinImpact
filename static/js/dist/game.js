332a561ffb369a060a976131eb48226c411a03d8
xiwen_youmu <1597083201@qq.com>
2022-12-19 23:08:26 +0800

    add interface of login and register

--- static/js/dist/game.js
+++ static/js/dist/game.js
@@ -735,7 +735,80 @@ class ScoreBoard {
         this.username = "";
         this.photo = "";
         if(this.root.AcWingOS) this.platform = "acapp";
-
+        this.$settings = $(`
+            <div class="xiwen-game-settings">
+                <div class="xiwen-game-settings-login">
+                    <div class="xiwen-game-settings-title">登陆</div>
+                    <div class="xiwen-game-settings-username">
+                        <div class="xiwen-game-settings-item">
+                            <input type="text" placeholder="用户名">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-password">
+                        <div class="xiwen-game-settings-item">
+                            <input type="password" placeholder="密码">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-submit">
+                        <div class="xiwen-game-settings-item">
+                            <button>开启提瓦特之旅</button>
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-error-message">
+                    </div>
+                    <div class="xiwen-game-settings-goto-register">
+                        注册
+                    </div>
+                    <br>
+                    <div class="xiwen-game-settings-acwing">
+                        <img width="30" src="https://app4220.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
+                        <div>
+                            Acwing一键登陆
+                        </div>
+                    </div>
+                </div>
+                <div class="xiwen-game-settings-register">
+                    <div class="xiwen-game-settings-title">注册</div>
+                    <div class="xiwen-game-settings-username">
+                        <div class="xiwen-game-settings-item">
+                            <input type="text" placeholder="用户名">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-password">
+                        <div class="xiwen-game-settings-item">
+                            <input type="password" placeholder="密码">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-password">
+                        <div class="xiwen-game-settings-item">
+                            <input type="password" placeholder="确认密码">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-submit">
+                        <div class="xiwen-game-settings-item">
+                            <button>降临未知的世界</button>
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-error-message">
+                    </div>
+                    <div class="xiwen-game-settings-goto-register">
+                        登陆
+                    </div>
+                    <br>
+                    <div class="xiwen-game-settings-acwing">
+                        <img width="30" src="https://app4220.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
+                        <div>
+                            Acwing一键登陆
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+        this.root.$xiwen_game.append(this.$settings);
+        this.$login = this.$settings.find(".xiwen-game-settings-login");
+        this.$login.hide();
+        this.$register = this.$settings.find(".xiwen-game-settings-register");
+        this.$register.show();
         this.start();
     }
 
@@ -743,11 +816,13 @@ class ScoreBoard {
         this.getinfo();
     }
     login() { //打开登陆界面
-
+        this.$register.hide();
+        this.$login.show();
     }
 
     register() {// 打开注册界面
-
+        this.$login.hide();
+        this.$register.show();
     }
 
     getinfo() {
@@ -766,18 +841,18 @@ class ScoreBoard {
                     outer.photo = resp.photo;
                     outer.root.menu.show();
                 } else {
-                    outer.login();
+                    outer.register();
                 }
             }
         });
     }
 
     hide() {
-        
+        this.$settings.hide();
     }
 
     show() {
-
+        this.$settings.show();
     }
 }export class xiwenGame {
     constructor(id, AcGameOS) {
--- static/js/src/settings/settings.js
+++ static/js/src/settings/settings.js
@@ -5,7 +5,80 @@ class Settings {
         this.username = "";
         this.photo = "";
         if(this.root.AcWingOS) this.platform = "acapp";
-
+        this.$settings = $(`
+            <div class="xiwen-game-settings">
+                <div class="xiwen-game-settings-login">
+                    <div class="xiwen-game-settings-title">登陆</div>
+                    <div class="xiwen-game-settings-username">
+                        <div class="xiwen-game-settings-item">
+                            <input type="text" placeholder="用户名">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-password">
+                        <div class="xiwen-game-settings-item">
+                            <input type="password" placeholder="密码">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-submit">
+                        <div class="xiwen-game-settings-item">
+                            <button>开启提瓦特之旅</button>
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-error-message">
+                    </div>
+                    <div class="xiwen-game-settings-goto-register">
+                        注册
+                    </div>
+                    <br>
+                    <div class="xiwen-game-settings-acwing">
+                        <img width="30" src="https://app4220.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
+                        <div>
+                            Acwing一键登陆
+                        </div>
+                    </div>
+                </div>
+                <div class="xiwen-game-settings-register">
+                    <div class="xiwen-game-settings-title">注册</div>
+                    <div class="xiwen-game-settings-username">
+                        <div class="xiwen-game-settings-item">
+                            <input type="text" placeholder="用户名">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-password">
+                        <div class="xiwen-game-settings-item">
+                            <input type="password" placeholder="密码">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-password">
+                        <div class="xiwen-game-settings-item">
+                            <input type="password" placeholder="确认密码">
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-submit">
+                        <div class="xiwen-game-settings-item">
+                            <button>降临未知的世界</button>
+                        </div>
+                    </div>
+                    <div class="xiwen-game-settings-error-message">
+                    </div>
+                    <div class="xiwen-game-settings-goto-register">
+                        登陆
+                    </div>
+                    <br>
+                    <div class="xiwen-game-settings-acwing">
+                        <img width="30" src="https://app4220.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
+                        <div>
+                            Acwing一键登陆
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+        this.root.$xiwen_game.append(this.$settings);
+        this.$login = this.$settings.find(".xiwen-game-settings-login");
+        this.$login.hide();
+        this.$register = this.$settings.find(".xiwen-game-settings-register");
+        this.$register.show();
         this.start();
     }
 
@@ -13,11 +86,13 @@ class Settings {
         this.getinfo();
     }
     login() { //打开登陆界面
-
+        this.$register.hide();
+        this.$login.show();
     }
 
     register() {// 打开注册界面
-
+        this.$login.hide();
+        this.$register.show();
     }
 
     getinfo() {
@@ -36,17 +111,17 @@ class Settings {
                     outer.photo = resp.photo;
                     outer.root.menu.show();
                 } else {
-                    outer.login();
+                    outer.register();
                 }
             }
         });
     }
 
     hide() {
-        
+        this.$settings.hide();
     }
 
     show() {
-
+        this.$settings.show();
     }
 }
